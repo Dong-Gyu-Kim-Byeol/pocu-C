a86a1730c1bcbbdf3d846de4c20703e424462859
LAP_4C8T\LAP <fwics88@gmail.com>
2022-03-20 19:58:54 +0900

    lab9

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,23 +1,21 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#include <memory.h>
+#define MAX(x, y) ((x) > (y)? (x) : (y))
 
-#define MAX(x, y) (x > y? x : y)
+#define MIN(x, y) ((x) < (y)? (x) : (y))
 
-#define MIN(x, y) (x < y? x : y)
+#define ABS(x) ((x) >= 0? (x) : (-x))
 
-#define ABS(x) (x >= 0? x : -x)
+#define RANGE(curr, min, max) if((min) <= (max)) for(curr = (min); curr <= (max); ++curr)
 
-#define RANGE(curr, min, max) if(min <= max) for(curr = min; curr <= max; ++curr)
-
-#define RANGE_DESC(curr, max, min) if(min <= max) for(curr = max; curr >= min; --curr)
+#define RANGE_DESC(curr, max, min) if((min) <= (max)) for(curr = (max); curr >= (min); --curr)
 
 #define SET(ary, start, count, value)   \
 {                                       \
     size_t i;                           \
-    for (i = 0; i < count; ++i) {       \
-        ary[start + i] = value;         \
+    for (i = 0; i < (count); ++i) {     \
+        (ary)[(start) + i] = (value);   \
     }                                   \
 }                                       \
 
